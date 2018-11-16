abeee994435440a49052ca0fe7af17d805cc729c
Glenn Gartner <glenngartner@gmail.com>
2018-11-16 15:13:22 -0500

    start ignoring cmake-build-debug and .idea

--- cmake-build-debug/external/glm/test/bug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspace/cpp/openGL/GPU_Rendering/external/glm/test/bug
-# Build directory: C:/workspace/cpp/openGL/GPU_Rendering/cmake-build-debug/external/glm/test/bug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test-bug_ms_vec_static "C:/workspace/cpp/openGL/GPU_Rendering/cmake-build-debug/external/glm/test/bug/test-bug_ms_vec_static.exe")
